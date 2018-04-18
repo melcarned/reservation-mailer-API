627667f51dd6b6687db91bd8158ba022d2b77976
melcarned <melcarned@gmail.com>
2018-04-18 02:56:42 -0400

    Updated home route to /

--- index.js
+++ index.js
@@ -27,7 +27,7 @@ app.use(bodyParser.json());
 app.set("port", process.env.PORT || 5000);
 app.use(express.static(__dirname + "/public"));
 
-app.get("/api", function(request, response) {
+app.get("/", function(request, response) {
   response.send("Welcome to Corsia Veloce's API!");
 });
 
